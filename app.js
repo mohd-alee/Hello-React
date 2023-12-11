5a6475de4811b14a7c019ff79e7198639a2482ca
Syed Muhammad Ali <muhammad.ali@tekrevol.com>
2023-12-11 20:22:56 +0500

    Beginning of swiggy clone

--- app.js
+++ app.js
@@ -1,49 +1,154 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "boxicons";
+import logo from "./assets/imgs/app-logo.png";
 
-const Heading = () => {
+// Components
+const Header = () => {
 	return (
-		<h1 className="someClass" data-key="value">
-			Hello JSX1
-		</h1>
-	);
-};
-const Paragraph = () => {
-	return (
-		<p>
-			Some Text <strong>Some bold Text</strong>
-		</p>
-	);
-};
-const Image = () => {
-	return (
-		<img
-			className="img-fluid"
-			src="https://miro.medium.com/v2/da:true/resize:fit:1160/1*t_G1kZwKv0p2arQCgYG7IQ.gif"
-		/>
-	);
-};
-const Card = () => {
-	return (
-		<div className="container">
-			<div className="row align-items-center">
-				<div className="card py-3">
-					<div className="row align-items-center">
-						<div className="col-6">
-							<Heading />
-							<Paragraph />
-						</div>
-						<div className="col-6">
-							<div className="someWrapper">
-								<Image />
-							</div>
+		<header className="site-header py-2">
+			<div className="container">
+				<div className="row align-items-center">
+					<div className="col-md-2">
+						<div className="logo-wrapper">
+							<a href="#">
+								<img src={logo} className="img-fluid" />
+							</a>
 						</div>
 					</div>
+					<div className="col-md-10">
+						<nav className="navbar navbar-expand-lg">
+							<button
+								className="navbar-toggler"
+								type="button"
+								data-bs-toggle="collapse"
+								data-bs-target="#navbarSupportedContent"
+								aria-controls="navbarSupportedContent"
+								aria-expanded="false"
+								aria-label="Toggle navigation"
+							>
+								<span className="navbar-toggler-icon"></span>
+							</button>
+							<div
+								className="collapse navbar-collapse"
+								id="navbarSupportedContent"
+							>
+								<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+									<li className="nav-item">
+										<a className="nav-link active" aria-current="page" href="#">
+											Home
+										</a>
+									</li>
+									<li className="nav-item">
+										<a className="nav-link" href="#">
+											About
+										</a>
+									</li>
+									<li className="nav-item">
+										<a className="nav-link" href="#">
+											Contact Us
+										</a>
+									</li>
+									<li className="nav-item">
+										<a className="nav-link" href="#">
+											<box-icon name="cart"></box-icon>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
+};
+const SearchBar = () => {
+	return (
+		<section className="search-wrapper my-3">
+			<div className="container">
+				<div className="row justify-content-end">
+					<div className="col-md-4">
+						<form className="d-flex flex-wrap" method="post">
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Search Restaurant"
+								required
+							/>
+							<input className="btn btn-primary" type="submit" value="Search" />
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+const RestaurantsWrapper = () => {
+	return (
+		<section className="restaurant-wrapper">
+			<div className="container">
+				<div className="row">
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+					<RestaurantCard />
+				</div>
+			</div>
+		</section>
+	);
+};
+const RestaurantCard = () => {
+	return (
+		<div className="col-md-3 mb-3">
+			<div className="single-res-card">
+				<div className="res-featured-image">
+					<img
+						src="https://images.deliveryhero.io/image/fd-pk/LH/hwd6-listing.jpg"
+						className="img-fluid"
+						alt="Restaurant Image"
+					/>
+				</div>
+				<div className="res-deets pt-3">
+					<h5 className="res-title">Restaurant Title</h5>
+					<p className="res-rating d-flex align-items-center">
+						<box-icon type="solid" name="star-half"></box-icon> 4.5 -
+						<box-icon name="timer"></box-icon> 30 mins
+					</p>
+					<p className="res-cuisine mb-0">
+						<span>Cuisines</span>: Burgers, Steaks, Sandwiches...
+					</p>
+					<p className="res-location mb-0">
+						<span>Location</span>: Hyderabad
+					</p>
 				</div>
 			</div>
 		</div>
 	);
 };
+// Components Ends
+
+const AppLayout = () => {
+	return (
+		<>
+			<Header />
+			<SearchBar />
+			<RestaurantsWrapper />
+		</>
+	);
+};
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Card />);
+root.render(<AppLayout />);
